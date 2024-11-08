b306df24a698f1c037d39951374dae369acf5212
revisa1 <revisa@rpi.edu>
2024-11-08 15:48:59 -0500

    changed name, test 4

--- labs/lab09/dbActors.sql
+++ labs/lab09/dbActors.sql
@@ -2,12 +2,12 @@
 CREATE TABLE `actors` (
    `actorid` int(10) unsigned NOT NULL AUTO_INCREMENT,
    `last_name` varchar(100) NOT NULL,
-   `first_names` varchar(4) DEFAULT NULL,
-   `dob` date(10)
+   `first_names` varchar(100) DEFAULT NULL,
+   `dob` varchar(10)
    PRIMARY KEY (`actorid`)
 );
 -- insert data into the tables
-INSERT INTO movies
+INSERT INTO actors
 VALUES (1, "Jones", "Indiana", "1959-11-19"),
    (2, "Johanessen", "Scarlet","1950-04-12"),
-   (3, "Pitt", "Brad" "1940-24-06");
+   (3, "Pitt", "Brad", "1940-24-06");
