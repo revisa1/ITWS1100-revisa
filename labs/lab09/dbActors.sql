d9d9190cfa7f4bfa14cd859b0481f726d39aaee5
revisa1 <revisa@rpi.edu>
2024-11-12 13:54:50 -0500

    added two more actors to actor tables, after 1960

--- labs/lab09/dbActors.sql
+++ labs/lab09/dbActors.sql
@@ -10,4 +10,6 @@ CREATE TABLE `actors` (
 INSERT INTO actors
 VALUES (1, "Jones", "Indiana", "1959-11-19"),
    (2, "Johanessen", "Scarlet","1950-04-12"),
-   (3, "Pitt", "Brad", "1940-24-06");
+   (3, "Pitt", "Brad", "1940-24-06"),
+   (4, "Cavell","Henry", "1970-20-02"),
+   (5, "Holland","Tom", "1984-08-08");
