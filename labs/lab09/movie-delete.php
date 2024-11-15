df05429e6b36de0d9062552c10a623289644368e
revisa1 <revisa@rpi.edu>
2024-11-14 23:18:22 -0500

    trying to fix actors remove button

--- labs/lab09/movie-delete.php
+++ labs/lab09/movie-delete.php
@@ -3,7 +3,7 @@
   
   /* Create a new database connection object, passing in the host, username,
      password, and database to use. The "@" suppresses errors. */
-  @$db = new mysqli('localhost', 'root', 'nlg7ejrB_', 'movies');
+  @$db = new mysqli('localhost', 'root', 'nlg7ejrB_', 'iit');
   
   if ($db->connect_error) {
     $connectErrors = array(
