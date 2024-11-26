cb8ce57b130a6f7660deaf3471a44bf777aa7d12
revisa1 <revisa@rpi.edu>
2024-11-26 12:41:16 -0500

    created database connection; made variables in globals array

--- Quiz3/includes/conn.php
+++ Quiz3/includes/conn.php
@@ -1,7 +1,7 @@
 <?php
-$user = 'root';
-$pwd='nlg7ejrB_';
-$db='mySite';
-$svr='localhost';
+$GLOBALS['user'] = 'root';
+$GLOBALS['pwd']='nlg7ejrB_';
+$GLOBALS['db']='mySite';
+$GLOBALS['svr']='localhost';
 $GLOBALS['iit']=str_repeat('../',substr_count(dirname($_SERVER['PHP_SELF']),'/')-1)
 ?>        
--- Quiz3/includes/headernav.php
+++ Quiz3/includes/headernav.php
@@ -33,6 +33,7 @@
     <div class="prjtLabsContent">
       
       <?php
+        @ $db=new mysqli($GLOBALS['svr'],$GLOBALS['user'],$GLOBALS['pwd'],$GLOBALS['db']);
         $relPath=$GLOBALS['iit'] . 'labs/'
         $query='select * from myLabs';
         $result=$db->query($query);
@@ -42,6 +43,7 @@
           echo '<a href="'. htmlspecialchars(trim($relPath)) . htmlspecialchars(trim($row['landing'])) . '">' .
           htmlspecialchars(trim($row['title'])) . '</a>';
         }
+        $db->close();
       ?>
 
     </div>
