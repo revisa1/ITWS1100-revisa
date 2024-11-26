05fb557d39814c819e2dfcd023dee9194d60b023
revisa1 <revisa@rpi.edu>
2024-11-26 12:31:56 -0500

    account for trailing slashes

--- Quiz3/includes/conn.php
+++ Quiz3/includes/conn.php
@@ -3,5 +3,5 @@ $user = 'root';
 $pwd='nlg7ejrB_';
 $db='mySite';
 $svr='localhost';
-$GLOBALS['iit']=str_repeat('../',substr_count(dirname($_SERVER['PHP_SELF']),'/'))
+$GLOBALS['iit']=str_repeat('../',substr_count(dirname($_SERVER['PHP_SELF']),'/')-1)
 ?>        
