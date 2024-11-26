d22f6e61bb6e9f52148e67ec3de96c4e8bccc969
revisa1 <revisa@rpi.edu>
2024-11-26 12:03:04 -0500

    fixed typeo; added a dash in footer

--- Quiz3/includes/footer.php
+++ Quiz3/includes/footer.php
@@ -2,7 +2,7 @@
       <h1><strong>Thank You For Visiting!</strong></h1>
       <div class="footerLinks">
         <a href="mailto:arielle.a.revis@gmail.com">arielle.a.revis@gmail.com</a>
-        <a href="tel:8482379843">(848) - 237 9843</a>
+        <a href="tel:8482379843">(848) - 237 - 9843</a>
         <a href="https://www.linkedin.com/in/arielle-revis/">linkedin.com/ariellerevis</a>
       </div>
     </div>
