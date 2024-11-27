41d9daa2f72dc381d0d47c18c87b82e612cee3a9
revisa1 <revisa@rpi.edu>
2024-11-27 15:44:02 -0500

    debug conn

--- Quiz3/includes/conn.php
+++ Quiz3/includes/conn.php
@@ -1,6 +1,7 @@
 <?php
 $GLOBALS['user'] = 'root';
 $GLOBALS['pwd']='nlg7ejrB_';
+
 $GLOBALS['db']='mySite';
 $GLOBALS['svr']='localhost';
 $GLOBALS['iit']=str_repeat('../',substr_count(dirname($_SERVER['PHP_SELF']),'/')-1);
