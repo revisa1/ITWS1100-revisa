658d96afd9694b6d674606757e42d084bbe8736f
revisa1 <revisa@rpi.edu>
2024-11-27 14:28:00 -0500

    testing a different variable that is not an  empoty string

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
   }
   $havePost=isset($_POST['save']);
   $errors='';
-  echo '<h4>' . $errors . '</h4>';
+  echo '<h4>' . $dbOk . '</h4></div>';
   
 //   if ($havePost) {
 //     $username = htmlspecialchars(trim($_POST["usernames"]));
