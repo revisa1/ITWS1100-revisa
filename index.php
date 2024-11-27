a367fb144df933b67b342038393fbd45fc40af05
revisa1 <revisa@rpi.edu>
2024-11-27 16:11:41 -0500

    debug login

--- index.php
+++ index.php
@@ -30,12 +30,13 @@
   
   
   if ($havePost) {
+
     $username = htmlspecialchars(trim($_POST["usernames"]));
     $password = htmlspecialchars(trim($_POST["passwords"]));
     $userType = htmlspecialchars(trim($_POST["userTypes"]));
     $labname = htmlspecialchars(trim($_POST['labnames']));
     $lablanding = htmlspecialchars(trim($_POST['lablandings']));
-    echo 'abc';      
+   
     $focusId = ''; 
      
     if ($username == '') {
@@ -58,51 +59,51 @@
     $errors .= '<li>Lab Landing may not be blank</li>';
     if ($focusId == '') $focusId = '#lablandings';
     }
-    echo 'xxx';
-    // if ($errors != '') {
-    //   echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
-    //   echo $errors;
-    //   echo '</ul></div>';
-    //   echo '<script type="text/javascript">';
-    //   echo '  $(document).ready(function() {';
-    //   echo '    $("' . $focusId . '").focus();';
-    //   echo '  });';
-    //   echo '</script>';
-    // } else {
-    //   if ($dbOk) {
-    //     $usernameForDb = trim($_POST["usernames"]);
-    //     $passwordForDb = trim($_POST["passwords"]);
-    //     $userTypeForDb = trim($_POST["userTypes"]);
-    //     $labnameForDb = trim($_POST['labnames']);
-    //     $lablandingForDb = trim($_POST['lablandings']);
+
+    if ($errors != '') {
+      echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
+      echo $errors;
+      echo '</ul></div>';
+      echo '<script type="text/javascript">';
+      echo '  $(document).ready(function() {';
+      echo '    $("' . $focusId . '").focus();';
+      echo '  });';
+      echo '</script>';
+    } else {
+      if ($dbOk) {
+        $usernameForDb = trim($_POST["usernames"]);
+        $passwordForDb = trim($_POST["passwords"]);
+        $userTypeForDb = trim($_POST["userTypes"]);
+        $labnameForDb = trim($_POST['labnames']);
+        $lablandingForDb = trim($_POST['lablandings']);
        
-    //     $insQuery = "insert into mySiteUsers (`username`,`pw`,`userType`) values(?,?,?)";//actors=table name, last_names,etc.. field names
-    //     $statement = $db->prepare($insQuery);
-    //     $statement->bind_param("sss", $usernameForDb, $passwordForDb, $userTypeForDb);
-    //     $statement->execute();
+        $insQuery = "insert into mySiteUsers (`username`,`pw`,`userType`) values(?,?,?)";//actors=table name, last_names,etc.. field names
+        $statement = $db->prepare($insQuery);
+        $statement->bind_param("sss", $usernameForDb, $passwordForDb, $userTypeForDb);
+        $statement->execute();
 
-    //     echo '<div class="messages"><h4>Welcome ' . $username . '!</h4>';
+        echo '<div class="messages"><h4>Welcome ' . $username . '!</h4>';
 
-    //     //$success=true;
+        //$success=true;
 
-    //     $statement->close();
+        $statement->close();
 
-    //     $insLabQuery='insert into myLabs(`title`,`landing`) values(?,?)';
-    //     $labStatement=$db->prepare($insLabQuery);
-    //     $labStatement->bind_param('ss',$labnameForDb,$lablandingForDb);
-    //     $labStatement->execute();              
+        $insLabQuery='insert into myLabs(`title`,`landing`) values(?,?)';
+        $labStatement=$db->prepare($insLabQuery);
+        $labStatement->bind_param('ss',$labnameForDb,$lablandingForDb);
+        $labStatement->execute();              
 
-    //     echo '<div class="messages"><h4>Added ' . $labname . 'to Labs!</h4>';
+        echo '<div class="messages"><h4>Added ' . $labname . 'to Labs!</h4>';
 
-    //     $labStatement->close();
+        $labStatement->close();
 
 
-    //   }
-    // }
+      }
+    }
  }
-// echo '<h4>' . $dbOk . '/<h4>'
-// ?>
-<!--     
+
+?>
+    
       
       <h3 id='loginHeader'>Login Form</h3>
       <form id="loginform" name="loginform" action="index.php" method="post" onsubmit="return checkForm(this);">
@@ -110,7 +111,7 @@
             <div class="formData">
 
               <label class="field" for="usernames">Username:</label>
-              <div class="value"><input type="text" size="60" value="<?php/* if ($havePost && $errors != '') {
+              <div class="value"><input type="text" size="60" value="<?php/if ($havePost && $errors != '') {
                                                                           echo $username;
                                                                         } ?>" name="usernames" id="usernames" /></div>
 
@@ -127,7 +128,7 @@
               <input type="submit" value="save" id="save" name="save" />
             </div>
         </fieldset>
-      </form> -->
+      </form>
 <!--
       <button id=logoutButton>Logout</button>
 
