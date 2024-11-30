bf533ef5cf56983141738980e7755b19d2fbdaca
revisa1 <revisa@rpi.edu>
2024-11-29 22:54:48 -0500

    put php at bottom

--- index.php
+++ index.php
@@ -11,135 +11,7 @@
       dynamically loading php information and databases!</h3>
 </div>
 
-<?php
 
-  $dbOk=false;
-  $loggedIn=false;
-  @$db=new mysqli($GLOBALS['svr'],$GLOBALS['user'],$GLOBALS['pwd'],$GLOBALS['database']);
-  //@$db = new mysqli('localhost', 'root', 'nlg7ejrB_', 'mySite');
-
-  if ($db->connect_error){
-    echo '<div class="messages">Could not connect to the database. Error: ';
-    echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
-  } else {
-    $dbOk = true;
-  }
-  $havePost=isset($_POST['save']);
-  $errors='';
-
-  
-  
-  if ($havePost) {
-
-    if(!$loggedIn){
-
-      
-      $username = htmlspecialchars(trim($_POST["usernames"]));
-      $password = htmlspecialchars(trim($_POST["passwords"]));
-      $userType = htmlspecialchars(trim($_POST["userTypes"]));
-      // $labname = htmlspecialchars(trim($_POST['labnames']));
-      // $lablanding = htmlspecialchars(trim($_POST['lablandings']));
-    
-      $focusId = ''; 
-      // if(basename($_SERVER['PHP_SELF'])=='index.php'){
-        if ($username == '') {
-          $errors .= '<li>Username may not be blank</li>';
-          if ($focusId == '') $focusId = '#usernames';
-        }
-        if ($password == '') {
-          $errors .= '<li>Password may not be blank</li>';
-          if ($focusId == '') $focusId = '#passwords';
-        }
-        if ($userType == '') {
-          $errors .= '<li>User Type may not be blank</li>';
-          if ($focusId == '') $focusId = '#userTypes';
-        }
-        
-        if ($errors != '') {
-          echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
-          echo $errors;
-          echo '</ul></div>';
-          echo '<script type="text/javascript">';
-          echo '  $(document).ready(function() {';
-          echo '    $("' . $focusId . '").focus();';
-          echo '  });';
-          echo '</script>';
-        } else {
-          if ($dbOk) {
-            $usernameForDb = trim($_POST["usernames"]);
-            $passwordForDb = trim($_POST["passwords"]);
-            $userTypeForDb = trim($_POST["userTypes"]);
-          
-            $insQuery = "insert into mySiteUsers (`username`,`pw`,`userType`) values(?,?,?)";//actors=table name, last_names,etc.. field names
-            $statement = $db->prepare($insQuery);
-            $statement->bind_param("sss", $usernameForDb, $passwordForDb, $userTypeForDb);
-            $statement->execute();
-
-            echo '<div class="messages"><h4>Welcome ' . $username . '!</h4>';
-
-            //$success=true;
-
-            $statement->close();
-            echo '<script type="text/JavaScript"> 
-              var loginForm=document.getElementById("loginform");
-              loginForm.style.display="none";
-              var labForm=document.getElementById("editlabs");
-              labForm.style.display="block"; 
-            </script> ';
-          }
-        }
-    }
-    // } elseif($loggedIn){
-    //   echo '<script type="text/JavaScript"> 
-    //       var loginForm=getElementById("loginform");
-    //       loginForm.style.display="block";
-    //       var labForm=getElementById("editlabs");
-    //       labForm.style.display="none";
-    //     } 
-    //   </script> '
-    //   $loggedIn=false;
-    // }
-
-    
-    // } elseif($_SERVER['PHP_SELF']=='updateLabs.php'){
-
-  //     if ($labname == '') {
-  //       $errors .= '<li>Lab Number may not be blank</li>';
-  //       if ($focusId == '') $focusId = '#labnames';
-  //     }
-  //     if ($lablanding == '') {
-  //     $errors .= '<li>Lab Landing may not be blank</li>';
-  //     if ($focusId == '') $focusId = '#lablandings';
-  //     }
-  //     if ($errors != '') {
-  //       echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
-  //       echo $errors;
-  //       echo '</ul></div>';
-  //       echo '<script type="text/javascript">';
-  //       echo '  $(document).ready(function() {';
-  //       echo '    $("' . $focusId . '").focus();';
-  //       echo '  });';
-  //       echo '</script>';
-  //     } else {
-  //       if ($dbOk) {
-  //         $labnameForDb = trim($_POST['labnames']);
-  //         $lablandingForDb = trim($_POST['lablandings']);
-        
-  //         $insLabQuery='insert into myLabs(`title`,`landing`) values(?,?)';
-  //         $labStatement=$db->prepare($insLabQuery);
-  //         $labStatement->bind_param('ss',$labnameForDb,$lablandingForDb);
-  //         $labStatement->execute();              
-  
-  //         echo '<div class="messages"><h4>Added ' . $labname . 'to Labs!</h4>';
-  
-  //         $labStatement->close();
-  //       }
-  //     }
-  // }
-       
-}
-
-?>
     
       
       <!-- <h3 id='loginHeader'>Login Form</h3> -->
@@ -228,4 +100,132 @@
         ?>
       </table>
        -->
+
+<?php
+
+  $dbOk=false;
+  $loggedIn=false;
+  @$db=new mysqli($GLOBALS['svr'],$GLOBALS['user'],$GLOBALS['pwd'],$GLOBALS['database']);
+  //@$db = new mysqli('localhost', 'root', 'nlg7ejrB_', 'mySite');
+
+  if ($db->connect_error){
+    echo '<div class="messages">Could not connect to the database. Error: ';
+    echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+  } else {
+    $dbOk = true;
+  }
+  $havePost=isset($_POST['save']);
+  $errors='';
+
+
+
+  if ($havePost) {
+
+    if(!$loggedIn){
+
+      
+      $username = htmlspecialchars(trim($_POST["usernames"]));
+      $password = htmlspecialchars(trim($_POST["passwords"]));
+      $userType = htmlspecialchars(trim($_POST["userTypes"]));
+      // $labname = htmlspecialchars(trim($_POST['labnames']));
+      // $lablanding = htmlspecialchars(trim($_POST['lablandings']));
+    
+      $focusId = ''; 
+      // if(basename($_SERVER['PHP_SELF'])=='index.php'){
+        if ($username == '') {
+          $errors .= '<li>Username may not be blank</li>';
+          if ($focusId == '') $focusId = '#usernames';
+        }
+        if ($password == '') {
+          $errors .= '<li>Password may not be blank</li>';
+          if ($focusId == '') $focusId = '#passwords';
+        }
+        if ($userType == '') {
+          $errors .= '<li>User Type may not be blank</li>';
+          if ($focusId == '') $focusId = '#userTypes';
+        }
+        
+        if ($errors != '') {
+          echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
+          echo $errors;
+          echo '</ul></div>';
+          echo '<script type="text/javascript">';
+          echo '  $(document).ready(function() {';
+          echo '    $("' . $focusId . '").focus();';
+          echo '  });';
+          echo '</script>';
+        } else {
+          if ($dbOk) {
+            $usernameForDb = trim($_POST["usernames"]);
+            $passwordForDb = trim($_POST["passwords"]);
+            $userTypeForDb = trim($_POST["userTypes"]);
+          
+            $insQuery = "insert into mySiteUsers (`username`,`pw`,`userType`) values(?,?,?)";//actors=table name, last_names,etc.. field names
+            $statement = $db->prepare($insQuery);
+            $statement->bind_param("sss", $usernameForDb, $passwordForDb, $userTypeForDb);
+            $statement->execute();
+
+            echo '<div class="messages"><h4>Welcome ' . $username . '!</h4>';
+
+            //$success=true;
+
+            $statement->close();
+            echo '<script type="text/JavaScript"> 
+              document.getElementById("loginform").style.display="none";
+              document.getElementById("editlabs").style.display="block";
+            </script> ';
+          }
+        }
+    }
+    // } elseif($loggedIn){
+    //   echo '<script type="text/JavaScript"> 
+    //       var loginForm=getElementById("loginform");
+    //       loginForm.style.display="block";
+    //       var labForm=getElementById("editlabs");
+    //       labForm.style.display="none";
+    //     } 
+    //   </script> '
+    //   $loggedIn=false;
+    // }
+
+    
+    // } elseif($_SERVER['PHP_SELF']=='updateLabs.php'){
+
+  //     if ($labname == '') {
+  //       $errors .= '<li>Lab Number may not be blank</li>';
+  //       if ($focusId == '') $focusId = '#labnames';
+  //     }
+  //     if ($lablanding == '') {
+  //     $errors .= '<li>Lab Landing may not be blank</li>';
+  //     if ($focusId == '') $focusId = '#lablandings';
+  //     }
+  //     if ($errors != '') {
+  //       echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
+  //       echo $errors;
+  //       echo '</ul></div>';
+  //       echo '<script type="text/javascript">';
+  //       echo '  $(document).ready(function() {';
+  //       echo '    $("' . $focusId . '").focus();';
+  //       echo '  });';
+  //       echo '</script>';
+  //     } else {
+  //       if ($dbOk) {
+  //         $labnameForDb = trim($_POST['labnames']);
+  //         $lablandingForDb = trim($_POST['lablandings']);
+        
+  //         $insLabQuery='insert into myLabs(`title`,`landing`) values(?,?)';
+  //         $labStatement=$db->prepare($insLabQuery);
+  //         $labStatement->bind_param('ss',$labnameForDb,$lablandingForDb);
+  //         $labStatement->execute();              
+
+  //         echo '<div class="messages"><h4>Added ' . $labname . 'to Labs!</h4>';
+
+  //         $labStatement->close();
+  //       }
+  //     }
+  // }
+      
+  }
+
+?>
 <?php include('Quiz3/includes/footer.php');?>
