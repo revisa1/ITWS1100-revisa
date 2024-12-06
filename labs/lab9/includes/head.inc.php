41d2d4d6e8c23fc3ece3be1b9d625c91d56dac4b
revisa1 <revisa@rpi.edu>
2024-12-06 11:29:04 -0500

    fixed header

--- labs/lab9/includes/head.inc.php
+++ labs/lab9/includes/head.inc.php
@@ -3,14 +3,50 @@
     <script type='text/javascript' src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
     <script type="text/javascript" src='<?php echo $GLOBALS['iit'] . "Quiz3/resources/mysite.js"?>'></script>
     <!-- <script type="text/javascript" src="Quiz3/resources/mysite.js"></script> -->
+    <script type="text/javascript" src="resources/iit.js"></script>
     <script type="text/javascript" src='<?php echo $GLOBALS['iit'] . "Resources/lab8jQuery.js"?>'></script>
     <!-- <script type="text/javascript" src="Resources/lab8jQuery.js"></script>  -->
     <link rel="stylesheet" href='<?php echo $GLOBALS['iit'] . "Resources/site.css"?>'>
     <!-- <link rel="stylesheet" href="Resources/site.css"> -->
+    <link href="resources/iit.css" rel="stylesheet" type="text/css"/>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <style>@import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap');</style>
   </head> 
+    <!-- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script type='text/javascript' src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
+    <script type="text/javascript" src="../../Quiz3/resources/mysite.js"></script>
+    <script type="text/javascript" src="resources/iit.js"></script>
+    <script type="text/javascript" src="../../Resources/lab8jQuery.js"></script> 
+    <link rel="stylesheet" href="../../Resources/site.css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">  
+    <link href="resources/iit.css" rel="stylesheet" type="text/css"/>
+    <style>@import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap');</style>
+  </head>
   <body>
+  <div class="header">
+      <a href ="../../aboutMe/aboutMe.html"><img src="../../Resources/logo.png" alt="Arielle Revis Logo"></a>
+      <div class="headerContents">
+        <h1><strong>Arielle Revis Portfolio</strong></h1>
+        <h2><em>The one-stop shop to all things 'Arielle'</em></h2>
+      </div>
+      <div class="ui-widget">
+        <label for="labsearch">Lab Search: </label>
+        <input id="labs">
+      </div>
+   </div>
+    <div class="navbar">
+      <div class="navLinks"><a href="../../aboutMe/aboutMe.html">About Me</a></div>
+      <div class="navLinks"><a href="../../Resume/rsmeLanding.html ">Resume</a></div>
+      <div class="prjtLabs">
+        <button class="prjtLabsBtn">Projects & Labs</button>
+        <div class="prjtLabsContent">
+        </div>
+      </div>
+      <div class="navLinks"><a href="http://revisarpi.eastus.cloudapp.azure.com/grpDir">Group Directory</a></div>
+      <div class="navLinks"><a href="../../contactInfo/cntct.html">Contact Info</a></div>
+    </div> -->
+    <body>
     <div class="header">
       <a class=aboutMeLink href='<?php echo $GLOBALS['iit'] . "aboutMe/aboutMe.php"?>'><img id='logoLink' src='<?php echo $GLOBALS['iit'] . 'Resources/logo.png'?>' alt="Arielle Revis Logo"></a>
       <div class="headerContents">
